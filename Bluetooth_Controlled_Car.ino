1c67cbed05bf23d0f2185f98fddeb891dd18ea00
Sai-Chakradhar <63908079+Sai-Chakradhar@users.noreply.github.com>
2020-04-18 17:41:09 +0530

    Update Bluetooth_Controlled_Car.ino

--- Bluetooth_Controlled_Car.ino
+++ Bluetooth_Controlled_Car.ino
@@ -1,33 +1,35 @@
 void stop()
 {
+  // Stoping the car
   digitalWrite(8,LOW);
   digitalWrite(9,LOW);
   digitalWrite(10,LOW);
   digitalWrite(11,LOW);
 }
 void setup() {
+  // Declaring pins 8,9,10,11 as output pins
   pinMode(8,OUTPUT);
   pinMode(9,OUTPUT);
   pinMode(10,OUTPUT);
   pinMode(11,OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(9600); //Braud rate:9600
 }
 
 void loop() {
   // Back
-  int Back=0,Straight=0,Left=0,right=1,x;
+  int Back=0,Straight=0,Left=0,right=0,x;
   if(Serial.available()>0)
   {
     Serial.print("hai");
-    x=Serial.read();
+    x=Serial.read();  // reading data serially
     Serial.println(x);
   if(x== 52){
   digitalWrite(8,HIGH);// back
   digitalWrite(9,LOW);
   digitalWrite(10,HIGH);
   digitalWrite(11,LOW);
-  delay(100);
-  stop();
+  delay(100); // It does the task for 100 sec(Moving back for 100 sec)
+  stop();  // Making stop
   }
   if(x == 49)
   {
